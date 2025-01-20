75b1c3512f9a1829eac83f313e60caa4b9bcb4d3
sureshpentapilli <121419611+sureshpentapilli@users.noreply.github.com>
2025-01-20 23:06:25 +0530

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -31,7 +31,7 @@ const Register = () => {
     }
 
     try {
-      const response = await axios.post('http://localhost:5000/auth/register', formData, {
+      const response = await axios.post('https://backend-3var.onrender.com/auth/register', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
